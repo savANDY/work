f5e06ce322b3743dec787895fbea1b929516ee65
savANDY <andicuu@gmail.com>
2019-06-24 18:24:06 +0200

    [1] Menu and header style

--- imports/ui/components/ListList.jsx
+++ imports/ui/components/ListList.jsx
@@ -6,6 +6,8 @@ import { NavLink } from 'react-router-dom';
 import i18n from 'meteor/universe:i18n';
 import BaseComponent from './BaseComponent.jsx';
 import { insert } from '../../api/lists/methods.js';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faBriefcase} from "@fortawesome/free-solid-svg-icons";
 
 export default class ListList extends BaseComponent {
   constructor(props) {
@@ -27,32 +29,70 @@ export default class ListList extends BaseComponent {
   render() {
     const { lists } = this.props;
     return this.renderRedirect() || (
-      <div className="list-todos">
-        <a className="link-list-new" onClick={this.createNewList}>
-          <span className="icon-plus" />
-          {i18n.__('components.listList.newList')}
-        </a>
-        {lists.map(list => (
-          <NavLink
-            to={`/lists/${list._id}`}
-            key={list._id}
-            title={list.name}
-            className="list-todo"
-            activeClassName="active"
-          >
-            {list.userId
-              ? <span className="icon-lock" />
-              : null}
-            {list.incompleteCount
-              ? <span className="count-list">{list.incompleteCount}</span>
-              : null}
-            {list.name}
-          </NavLink>
-        ))}
-      </div>
+        <ul className="navbar-nav">
+
+          <li className="nav-item">
+            <a className="nav-link collapsed" href="#" data-toggle="collapse"
+               data-target="#collapsePages" aria-expanded="true" aria-controls="collapsePages">
+              <FontAwesomeIcon icon={faBriefcase}/>
+              <span>To-do</span>
+            </a>
+            <div id="collapsePages" className="collapse" aria-labelledby="headingPages"
+                 data-parent="#accordionSidebar">
+              <div className="bg-white py-2 collapse-inner rounded list-todos">
+                <h6 className="collapse-header">Lists:</h6>
+                {lists.map(list => (
+                    <NavLink
+                        to={`/lists/${list._id}`}
+                        key={list._id}
+                        title={list.name}
+                        className="collapse-item"
+                        activeClassName="active"
+                      >
+                        {list.userId
+                          ? <span className="icon-lock" />
+                          : null}
+                        {list.incompleteCount
+                          ? <span className="count-list">{list.incompleteCount}</span>
+                          : null}
+                        {list.name}
+                      </NavLink>
+                    ))}
+                <div className="collapse-divider"/>
+                <h6 className="collapse-header">New list:</h6>
+                <a className="collapse-item" onClick={this.createNewList}>{i18n.__('components.listList.newList')}</a>
+              </div>
+            </div>
+          </li>
+
+
+        </ul>
     );
   }
 }
+      {/*<div className="list-todos">*/}
+      {/*  <a className="link-list-new" onClick={this.createNewList}>*/}
+      {/*    <span className="icon-plus" />*/}
+      {/*    {i18n.__('components.listList.newList')}*/}
+      {/*  </a>*/}
+      {/*  {lists.map(list => (*/}
+      {/*    <NavLink*/}
+      {/*      to={`/lists/${list._id}`}*/}
+      {/*      key={list._id}*/}
+      {/*      title={list.name}*/}
+      {/*      className="list-todo"*/}
+      {/*      activeClassName="active"*/}
+      {/*    >*/}
+      {/*      {list.userId*/}
+      {/*        ? <span className="icon-lock" />*/}
+      {/*        : null}*/}
+      {/*      {list.incompleteCount*/}
+      {/*        ? <span className="count-list">{list.incompleteCount}</span>*/}
+      {/*        : null}*/}
+      {/*      {list.name}*/}
+      {/*    </NavLink>*/}
+      {/*  ))}*/}
+      {/*</div>*/}
 
 ListList.propTypes = {
   lists: PropTypes.array,
--- imports/ui/layouts/App.jsx
+++ imports/ui/layouts/App.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import React, {Component} from 'react';
+import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom';
 import PropTypes from 'prop-types';
-import { TransitionGroup, CSSTransition } from 'react-transition-group';
-import { Meteor } from 'meteor/meteor';
+import {TransitionGroup, CSSTransition} from 'react-transition-group';
+import {Meteor} from 'meteor/meteor';
 
-import { Lists } from '../../api/lists/lists.js';
+import {Lists} from '../../api/lists/lists.js';
 import UserMenu from '../components/UserMenu.jsx';
 import ListList from '../components/ListList.jsx';
 import LanguageToggle from '../components/LanguageToggle.jsx';
@@ -14,21 +14,12 @@ import ListPageContainer from '../containers/ListPageContainer.jsx';
 import AuthPageSignIn from '../pages/AuthPageSignIn.jsx';
 import AuthPageJoin from '../pages/AuthPageJoin.jsx';
 import NotFoundPage from '../pages/NotFoundPage.jsx';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import {faBriefcase, faFolder} from '@fortawesome/free-solid-svg-icons'
 
 const CONNECTION_ISSUE_TIMEOUT = 5000;
 
 export default class App extends Component {
-  static getDerivedStateFromProps(nextProps) {
-    // Store a default list path that can be redirected to from "/" when
-    // the list is ready.
-    const newState = { defaultList: null, redirectTo: null };
-    if (!nextProps.loading) {
-      const list = Lists.findOne();
-      newState.defaultList = `/lists/${list._id}`;
-    }
-    return newState;
-  }
-
   constructor(props) {
     super(props);
     this.state = {
@@ -41,10 +32,21 @@ export default class App extends Component {
     this.logout = this.logout.bind(this);
   }
 
+  static getDerivedStateFromProps(nextProps) {
+    // Store a default list path that can be redirected to from "/" when
+    // the list is ready.
+    const newState = {defaultList: null, redirectTo: null};
+    if (!nextProps.loading) {
+      const list = Lists.findOne();
+      newState.defaultList = `/lists/${list._id}`;
+    }
+    return newState;
+  }
+
   componentDidMount() {
     setTimeout(() => {
       /* eslint-disable react/no-did-mount-set-state */
-      this.setState({ showConnectionIssue: true });
+      this.setState({showConnectionIssue: true});
     }, CONNECTION_ISSUE_TIMEOUT);
   }
 
@@ -60,13 +62,13 @@ export default class App extends Component {
   }
 
   renderRedirect(location) {
-    const { redirectTo, defaultList } = this.state;
-    const { pathname } = location;
+    const {redirectTo, defaultList} = this.state;
+    const {pathname} = location;
     let redirect = null;
     if (redirectTo && redirectTo !== pathname) {
-      redirect = <Redirect to={redirectTo} />;
+      redirect = <Redirect to={redirectTo}/>;
     } else if (pathname === '/' && defaultList) {
-      redirect = <Redirect to={defaultList} />;
+      redirect = <Redirect to={defaultList}/>;
     }
     return redirect;
   }
@@ -79,70 +81,82 @@ export default class App extends Component {
       menuOpen,
       loading,
     } = this.props;
-    const { showConnectionIssue } = this.state;
+    const {showConnectionIssue} = this.state;
 
     const commonChildProps = {
       menuOpen: this.props.menuOpen,
     };
 
     return (
-      <div id="container" className={menuOpen ? 'menu-open' : ''}>
-        <section id="menu">
-          <LanguageToggle />
-          <UserMenu user={user} logout={this.logout} />
-          <ListList lists={lists} />
-        </section>
-        {showConnectionIssue && !connected
-          ? <ConnectionNotification />
-          : null}
-        <div className="content-overlay" onClick={this.closeMenu} />
-        <div id="content-container">
-          {loading ? (
-            <Loading key="loading" />
-          ) : (
-            <TransitionGroup>
-              <CSSTransition
-                key={location.key}
-                classNames="fade"
-                timeout={200}
-              >
-                <Switch location={location}>
-                  <Route
-                    path="/lists/:id"
-                    render={({ match }) => (
-                      <ListPageContainer match={match} {...commonChildProps} />
-                    )}
-                  />
-                  <Route
-                    path="/signin"
-                    render={() => <AuthPageSignIn {...commonChildProps} />}
-                  />
-                  <Route
-                    path="/join"
-                    render={() => <AuthPageJoin {...commonChildProps} />}
-                  />
-                  <Route
-                    path="/*"
-                    render={() => <NotFoundPage {...commonChildProps} />}
-                  />
-                </Switch>
-              </CSSTransition>
-            </TransitionGroup>
-          )}
+        <div id="container" className={menuOpen ? 'menu-open' : ''}>
+          <section id="menu">
+
+
+            <a className="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+              <div className="sidebar-brand-icon">
+                <FontAwesomeIcon icon={faBriefcase}/>
+              </div>
+              <div className="sidebar-brand-text mx-3">Work</div>
+            </a>
+
+
+            <LanguageToggle/>
+            <UserMenu user={user} logout={this.logout}/>
+
+
+            <ListList lists={lists}/>
+          </section>
+          {showConnectionIssue && !connected
+              ? <ConnectionNotification/>
+              : null}
+          <div className="content-overlay" onClick={this.closeMenu}/>
+          <div id="content-container">
+            {loading ? (
+                <Loading key="loading"/>
+            ) : (
+                <TransitionGroup>
+                  <CSSTransition
+                      key={location.key}
+                      classNames="fade"
+                      timeout={200}
+                  >
+                    <Switch location={location}>
+                      <Route
+                          path="/lists/:id"
+                          render={({match}) => (
+                              <ListPageContainer match={match} {...commonChildProps} />
+                          )}
+                      />
+                      <Route
+                          path="/signin"
+                          render={() => <AuthPageSignIn {...commonChildProps} />}
+                      />
+                      <Route
+                          path="/join"
+                          render={() => <AuthPageJoin {...commonChildProps} />}
+                      />
+                      <Route
+                          path="/*"
+                          render={() => <NotFoundPage {...commonChildProps} />}
+                      />
+                    </Switch>
+                  </CSSTransition>
+                </TransitionGroup>
+            )}
+          </div>
         </div>
-      </div>
     );
   }
 
   render() {
     return (
-      <BrowserRouter>
-        <Route
-          render={({ location }) => (
-            this.renderRedirect(location) || this.renderContent(location)
-          )}
-        />
-      </BrowserRouter>
+        <BrowserRouter>
+          <Route
+              render={({location}) => (
+                  this.renderRedirect(location) || this.renderContent(location)
+              )}
+          />
+        </BrowserRouter>
     );
   }
 }
